8c526cffbb05c2a21be2a983bd5c924d4ea3cdca
vincenzo.frappietro <vincenzo.frappietro@hotmail.com>
2016-01-19 18:43:57 +0100

    Modified Initialize method

--- TestConsoleOnGit/Infrastructure.cs
+++ TestConsoleOnGit/Infrastructure.cs
@@ -13,7 +13,7 @@ namespace TestConsoleOnGit
 			int max = 0;
 
 			for (int i = 0; i < max; i++)
-				Console.WriteLine(string.Format("Initializing part {0}", i+1));
+				Console.WriteLine(string.Format("Initializing part {0} of {1}", i+1, max));
 		}
 	}
 }
